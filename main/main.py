1e660ae11cf1e1158a55338bc408cc5089e8b8b2
NeteR_VezeR <139502574+0netervezer0@users.noreply.github.com>
2025-09-25 22:13:35 +0300

    Fixed word length checking
    Fixed word length checking in main function;
    Improved variable initialization

--- main/main.py
+++ main/main.py
@@ -12,7 +12,7 @@ def clear() -> None:
     else:
         system( "clear" )
 
-def has_uppercase( word ) -> bool:
+def has_uppercase( word: str ) -> bool:
     """
     Checks if the word has uppercase;
     Returns true if it has uppercase
@@ -25,7 +25,7 @@ def arg_analysis() -> None:
     Gets the number of arguments and their values;
     Sets values to global variables
     """
-    global linesCnt, tipsCnt, ERR
+    global linesCnt, tipsCnt
 
     # Handle --help case
     if argc == 2 and argv[1] == "--help":
@@ -41,7 +41,7 @@ def arg_analysis() -> None:
         exit()
 
     # Process arguments
-    i = 1
+    i: int = 1
     while i < argc:
         if argv[i] == "-L":
             if i + 1 >= argc:
@@ -84,43 +84,44 @@ def arg_analysis() -> None:
             exit()
 
 
-# noinspection PyUnboundLocalVariable
 def main() -> int:
     """
-    Main function
+    Main function;
+    Returns exit code
     """
     global tipsCnt
-    if argc != 1: arg_analysis()
+    if argc != 1:
+        arg_analysis()
     clear()
     while True:
-        seted_word = input( "\x1b[34mSetter#\x1b[0m Set the word: " )
-        if seted_word == "!q" or seted_word == "!exit":
+        set_word: str = input( "\x1b[34mSetter#\x1b[0m Set the word: " )
+        if set_word == "!q" or set_word == "!exit":
             clear()
-            exit()
-        if not seted_word.isalpha():
+            return 1
+        if not set_word.isalpha():
             print( "\x1b[31mERROR:\x1b[0m Word couldn't contain special characters." )
             return 1
         else:
-            if 13 < len( seted_word ) < 2:
+            if len( set_word ) > 12 or len( set_word ) < 2:
                 print( "\x1b[31mERROR:\x1b[0m Word can't be longer than 12 characters and lower than 2." )
                 return 1
-            elif has_uppercase( seted_word ):
+            elif has_uppercase( set_word ):
                 print( "\x1b[31mERROR:\x1b[0m All characters should be lowercase." )
                 return 1
             else:
-                frame = [ f"{ "? "*len( seted_word ) }",
-                          f"{ "_ "*len( seted_word ) }" ]
+                frame: list = [ f"{ "? "*len( set_word ) }",
+                                f"{ "_ "*len( set_word ) }" ]
                 for line in range( linesCnt ):
-                    frame.append( f"{ "* "*len( seted_word ) }" )
+                    frame.append( f"{ "* "*len( set_word ) }" )
 
-                cnt = -1
+                cnt: int = -1
                 while cnt < linesCnt-1:
                     clear()
                     for s in frame:
                         print( s )
 
-                    word = input( '\n\x1b[34mGuesser#\x1b[0m Enter a word: ' )
-                    used_tips = 0
+                    word: str = input( '\n\x1b[34mGuesser#\x1b[0m Enter a word: ' )
+                    used_tips: int = 0
 
                     if word == "!q" or word == "!exit":
                         clear()
@@ -131,63 +132,62 @@ def main() -> int:
 
                     elif word == "!tip":
                         if tipsCnt > 0:
-                            hidden = [i for i in range( 0, len( seted_word )) if frame[0][i * 2] == "?"]
+                            hidden: list = [i for i in range( 0, len( set_word )) if frame[0][i * 2] == "?"]
                             if len( hidden ) > 0:
-                                if ( len( seted_word ) - used_tips ) >= 2:
+                                if ( len( set_word ) - used_tips ) >= 2:
                                     tipsCnt -= 1
                                     used_tips += 1
                                     rnd_i = hidden[randint( 0, len( hidden ) - 1 )]
                                     frame0 = list( frame[0] )
-                                    frame0[rnd_i * 2] = seted_word[rnd_i]
+                                    frame0[rnd_i * 2] = set_word[rnd_i]
                                     frame[0] = "".join( frame0 )
 
-                    elif len( word ) != len( seted_word ) or has_uppercase( word ) or not word.isalpha():
+                    elif len( word ) != len( set_word ) or has_uppercase( word ) or not word.isalpha():
                         pass
 
                     else:
                         cnt += 1
-                        obj = ""
-                        iteration = 0
+                        obj: str = ""
+                        iteration: int = 0
                         for c in word:
-                            if c == seted_word[iteration]:
+                            if c == set_word[iteration]:
                                 obj += f"\x1b[32m{ c }\x1b[0m "
-                            elif c in seted_word:
+                            elif c in set_word:
                                 obj += f"\x1b[33m{ c }\x1b[0m "
                             else:
                                 obj += f"\x1b[31m{ c }\x1b[0m "
                             iteration += 1
 
-                        if word == seted_word:
+                        if word == set_word:
                             clear()
-                            frame0 = ""
-                            for c in seted_word:
+                            frame0: str = ""
+                            for c in set_word:
                                 frame0 += f"{ c } "
                             frame[0] = frame0
                             frame[2 + cnt] = obj
                             for s in frame:
                                 print( s )
-                            print( f"\nGuesser win! The word was: { seted_word }" )
+                            print( f"\nGuesser win! The word was: { set_word }" )
                             break
 
                         frame[2+cnt] = obj
 
-                if word != seted_word:
+                if word != set_word:
                     clear()
-                    frame0 = ""
-                    for c in seted_word:
+                    frame0: str = ""
+                    for c in set_word:
                         frame0 += f"{ c } "
                     frame[0] = frame0
                     for s in frame:
                         print( s )
-                    print( f"\nSetter win! The word was: { seted_word }" )
+                    print( f"\nSetter win! The word was: { set_word }" )
 
 
 if __name__ == "__main__":
-    argc = len( argv )  # Count of arguments
+    argc:     int = len( argv )  # Count of arguments
 
     # Global vars
-    ERR = 0
-    linesCnt = 5
-    tipsCnt = 0
+    linesCnt: int = 5
+    tipsCnt:  int = 0
 
     main()
